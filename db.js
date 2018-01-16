94e7e869b1ece5242d5a6eab5893ec8cf8364706
SagiSan <samir.sagi@gmail.com>
2018-01-16 14:36:31 +0100

    update

--- db.js
+++ db.js
@@ -1,14 +1,79 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('mop_backend', 'root', 'dzbrasno', {
+  host: 'localhost',
+  dialect: 'mysql',
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+});
 
-var mysql = require('mysql')
+const User = sequelize.define('user', {
+    username: { type: Sequelize.STRING,  allowNull: false },
+    password: { type: Sequelize.STRING,  allowNull: false },
+    email:    { type: Sequelize.STRING,  allowNull: false },
+    isAdmin:  { type: Sequelize.BOOLEAN, allowNull: false }
+});
 
-module.exports = (onErrorCallback) => {
-    var connection = mysql.createConnection({
-        host     : 'localhost',
-        user     : 'root',
-        password : 'dzbrasno',
-        database : 'mop'
-      });
-    connection.connect();
-    connection.on("error", onErrorCallback);
-    return connection;
-};
+const Questionnaire = sequelize.define('questionnaire', {
+    title:    { type: Sequelize.STRING,  allowNull: false }
+});
+
+const Question = sequelize.define('question', {
+    question: { type: Sequelize.STRING,  allowNull: false },
+    type:     { type: Sequelize.INTEGER, allowNull: false }
+});
+Question.Questionnaire = Question.belongsTo(Questionnaire, {foreignKey: { allowNull: false }});
+
+const Choice = sequelize.define('choice', {
+    answer:   { type: Sequelize.STRING,  allowNull: false }
+});
+Choice.Question = Choice.belongsTo(Question, {foreignKey: { allowNull: false }});
+
+const Answer = sequelize.define('answer', {
+    answerText:   { type: Sequelize.STRING,  allowNull: true },
+    answerYesNo:  { type: Sequelize.BOOLEAN, allowNull: true }
+});
+Answer.Choice = Answer.belongsTo(Choice, { as: 'answerSingle', foreignKey: { allowNull: true }});
+Answer.Question = Answer.belongsTo(Question, {foreignKey: { allowNull: false }});
+Answer.Questionnaire = Answer.belongsTo(Questionnaire, {foreignKey: { allowNull: false }});
+Answer.User = Answer.belongsTo(User, {foreignKey: { allowNull: false }});
+
+const AnswerMultiple = sequelize.define('answerMultiple', {});
+AnswerMultiple.Choice = AnswerMultiple.belongsTo(Choice, {foreignKey: {allowNull: false}});
+AnswerMultiple.Answer = AnswerMultiple.belongsTo(Answer, {foreignKey: {allowNull: false}});
+
+
+User.Answers = User.hasMany(Answer);
+Questionnaire.Questions = Questionnaire.hasMany(Question);
+Question.Choices = Question.hasMany(Choice);
+Answer.AnswerMultipleChoices = Answer.hasMany(AnswerMultiple);
+
+sequelize.authenticate()
+    .then(() => {
+        console.log("Connected to database");
+        sequelize.sync()
+        .then(() => {
+            console.log("Database is set up");
+        })
+        .catch((error) => {
+            console.error("Database setup error: " + error);
+            process.exit(1);
+        });
+    })
+    .catch(err => {
+        console.error("Error connecting to database: " + err);
+        process.exit(1);
+    });
+
+module.exports = {
+    sequelize,
+    User,
+    Questionnaire,
+    Question,
+    Choice,
+    Answer,
+    AnswerMultiple
+}
--- routes/questionnaire.js
+++ routes/questionnaire.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
-var connectionCreator = require("../db.js");
-
+var db = require("../db.js");
+/*
 function createChoices(question, connection) {
   var promises = [];
   for (const choice of question.choices) {
@@ -143,6 +143,44 @@ router.post("/create", (req, res, next) => {
 router.get("/", async (req, res, next) => {
   const connection = connectionCreator(errorCatcher)
   res.send(await getQuestionnaires(connection));
+});*/
+
+const errorCatcher = (res, sendFullError) => (error) => {
+  console.error("Got error: " + JSON.stringify(error));
+  if (!res.headersSent) {
+    res.status(400);
+    if (sendFullError) {
+      res.send(error);
+    } else {
+      res.send();
+    }
+  };
+}
+
+router.get("/", (req, res) => {
+  db.Questionnaire.findAll({
+    include: [{
+      association: db.Questionnaire.Questions,
+      include: [ db.Question.Choices ]
+    }]
+  })
+  .then(results => {
+    res.send(results);
+  })
+  .catch(errorCatcher(res));
+});
+
+router.post("/create", (req, res, next) => {
+  const questionnaire = req.body;
+  db.Questionnaire.create(req.body, {
+    include: [{
+      association: db.Questionnaire.Questions,
+      include: [ db.Question.Choices ]
+    }]
+  }).then(() => {
+    res.send("OK");
+  })
+  .catch(errorCatcher(res, true));
 });
 
 module.exports = router;
