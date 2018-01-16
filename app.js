6dbe0bc57787e2fa3d7392367881b7543623c33e
SagiSan <samir.sagi@gmail.com>
2018-01-16 20:53:32 +0100

    update

--- app.js
+++ app.js
@@ -32,11 +32,8 @@ app.use(function (req, res, next) {
 app.use('/api/questionnaire', questionnaire);
 app.use('/api/user', user);
 
-// catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 // error handler
